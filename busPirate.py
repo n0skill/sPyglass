7405e957d4406149a4fbb75d3e6454b28fa9700e
noskill <marc.savioz@gmail.com>
2016-09-07 23:41:00 +0200

    Testing various timeout values

--- busPirate.py
+++ busPirate.py
@@ -7,39 +7,18 @@ class Capture:
 	def __init__(self, values):
 		# Capture is an array of values
 		self.values = values
-
-
-def recv(port):
-	try:
-		ser	= serial.Serial(port, 115200, timeout=5)
-		recv = ''
-		print('Listening...')
-		time.sleep(1)
-		if ser.is_open:
-			while ser.inWaiting() > 0:
-				recv += ser.readline()
-		print(recv)
-	except serial.SerialException as e:
-		if e.errno == 2:
-			print('Could not find device. Check spelling.')
-		if e.errno == 16:
-			print('The device is busy. Stop other programs using it')
-		else:
-			print(e)
-
 def send_cmd(port, cmd):
 	print('Sending cmd: ' + str(cmd) + ' to ' + str(port))
 	cmd = cmd+'\r' # Carriage return to execute command
 	try:
-		ser	= serial.Serial(port, 115200, timeout=5)
+		ser	= serial.Serial(port, 115200, timeout=500)
 		if ser.is_open:
 			print('open!')
 			ser.write(cmd.encode())
-			time.sleep(5)
+			time.sleep(10)
 			recv = ""
 			while ser.inWaiting() > 0:
 				recv += ser.read().decode("utf-8")
-			#print(recv)
 			ser.close()
 			return recv
 
@@ -57,11 +36,11 @@ def capture_voltage(port, time):
 	for i in range(0, time):
 		cmd += voltage_cmd
 	values = send_cmd(port, cmd)
-	#print(values)
 	regexified = re.findall('^(GND.+)$', values, re.MULTILINE)
-	for val in regexified:
-		if val.endswith('L\t\r') or val.endswith('H\t\r'):
-			print(val)
+	print(values)
+	#	print(val)
+		# if val.endswith('L\t\r') or val.endswith('H\t\r'):
+		# 	print(val)
 	e = Capture(values)
 if __name__ is '__main__':
 	capture_voltage(10)

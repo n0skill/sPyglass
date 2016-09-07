92494d0570f7d80e5af79132fc3bda835a8b0dc8
noskill <marc.savioz@gmail.com>
2016-09-07 16:28:14 +0200

    Add voltage reading function. Some various tests

--- busPirate.py
+++ busPirate.py
@@ -1,9 +1,51 @@
-def main(time):
-	voltage_cmd = 'v'
+# TODO: move all communication handling in this file/class
+import serial
+import time
+def recv(port):
+	try:
+		ser	= serial.Serial(port, 115200, timeout=5)
+		recv = ''
+		print('Listening...')
+		time.sleep(1)
+		if ser.is_open:
+			while ser.inWaiting() > 0:
+				recv += ser.readline()
+		print(recv)
+	except serial.SerialException as e:
+		if e.errno == 2:
+			print('Could not find device. Check spelling.')
+		if e.errno == 16:
+			print('The device is busy. Stop other programs using it')
+		else:
+			print(e)
+
+def send_cmd(port, cmd):
+	print('Sending cmd: ' + str(cmd) + ' to ' + str(port))
+	cmd = cmd+'\r' # Carriage return to execute command
+	try:
+		ser	= serial.Serial(port, 115200, timeout=5)
+		if ser.is_open:
+			print('open!')
+			ser.write(cmd.encode())
+			time.sleep(2)
+			recv = ""
+			while ser.inWaiting() > 0:
+				recv += ser.read().decode("utf-8")
+			print(recv)
+			ser.close()
+	except serial.SerialException as e:
+		if e.errno == 2:
+			print('Could not find device. Check spelling.')
+		if e.errno == 16:
+			print('The device is busy. Stop other programs using it')
+		else:
+			print(e)
+
+def capture_voltage(port, time):
+	voltage_cmd = 'v%'
 	cmd=""
 	for i in range(0, time):
-		cmd += voltage_cmd+'%'
-	print(cmd)
-
+		cmd += voltage_cmd
+	send_cmd(port, cmd)
 if __name__ is '__main__':
 	capture_voltage(10)
--- main.py
+++ main.py
@@ -5,25 +5,7 @@ import busPirate
 
 def main():
 	port 	= sys.argv[1]
-	try:
-		ser	= serial.Serial(port, 115200, timeout=5)
-		if ser.is_open:
-			print('open!')
-			ser.write('\r'.encode()) # We have to send an \r to get to the menu
-			ser.write('?\r'.encode())# Tesing: get the menu
-			out = ''
-			time.sleep(1)
-			while ser.inWaiting() > 0:
-				out += str(ser.readline())
-			print(out)
-			ser.close()
-			print('done.')
-			busPirate.main(10)
-	except serial.SerialException as e:
-		if e.errno == 2:
-			print('Could not find device. Check spelling.')
-		if e.errno == 16:
-			print('The device is busy. Stop other programs using it')
-		else:
-			print(e)
+	#busPirate.send_cmd(port, '\r')
+	busPirate.capture_voltage(port, 1)
+
 main()

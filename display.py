5d7be296b6c291f7b704f19536ee2e645e152a4f
noskill <marc.savioz@gmail.com>
2016-09-10 01:06:51 +0200

    Move all that should belong to the __init__ of Channel class there.

--- display.py
+++ display.py
@@ -18,7 +18,7 @@ class Channel:
 	nb = 0
 	height  = 80
 	def __init__(self, display, values, color, unit):
-		margin_l = 100
+		self.margin_l = 100
 		self.ampli = 30
 		self.zoom_level = 0
 		self.length = len(values)
@@ -28,52 +28,45 @@ class Channel:
 		self.values = values
 		self.display = display
 		self.nb = Channel.nb
-		cap_tuples = []
-
 		# Scale x axis
-		self.x_scale = (self.display.get_width()-margin_l)/self.length
-		self.max = max(values)
-		self.min = min(values)
+		self.x_scale = (self.display.get_width()-self.margin_l)/self.length
+		self.max = max(self.values, key=lambda y: y[1])[1]
+		self.min = min(self.values, key=lambda y: y[1])[1]
 
 		self.surface = pygame.Surface((display.get_width(), self.height))
 		self.surface.fill((25, 25, 25))
 		self.nb = Channel.nb
-		Channel.nb = Channel.nb+1
 
-	def plot(self):
-		margin_l = 100
-		# Add scale to axises
-		pygame.draw.line(self.surface, self.color, (margin_l, 2), (self.display.get_width(), 2), 1)
+		pygame.draw.line(self.surface, self.color, (self.margin_l, 2), (self.display.get_width(), 2), 1)
 		for i in range(0, self.length):
 			if i%5 == 0:
-				pygame.draw.line(self.surface, self.color, (i*self.x_scale+margin_l, 0), (i*self.x_scale+margin_l, 4), 1)
-		# Add labels to axises
+				pygame.draw.line(self.surface, self.color, (i*self.x_scale+self.margin_l, 0), (i*self.x_scale+self.margin_l, 4), 1)
+
 		pygame.font.init()
 		font = pygame.font.Font(None, 18)
 		labelmax = font.render(str(self.max), 1, self.color)
 		labelmin = font.render(str(self.min), 1, self.color)
+
 		self.lab1 = font.render("ch"+str(Channel.nb), 1, self.color)
+		self.surface.blit(self.lab1, (40, Channel.height/2))
+
 		self.timescale = font.render(str(self.length), 1, self.color)
 		self.surface.blit(self.timescale, (self.display.get_width()-20, 10))
-		self.surface.blit(self.lab1, (10, Channel.height/2))
-		self.surface.blit(labelmax, (margin_l, 10))
-		self.surface.blit(labelmin, (margin_l, 70))
 
-		# FIXME: invert line
-		#pygame.draw.lines(self.surface, self.color, False, cap_tuples, 2)
-		self.display.blit(self.surface, (0, self.nb*Channel.height))
-		cap_tuples = []
-		for id, val in enumerate(self.values):
-			scaled_val = (val-min(self.values))/(max(self.values)-min(self.values)+0.01)
-			cap_tuples.append((id*self.x_scale+100, ((self.height-10)*scaled_val)))
-			if id > 1:
-				pygame.draw.lines(self.surface, self.color, False, cap_tuples, 2)
-				self.display.blit(self.surface, (0, self.nb*Channel.height))
-				pygame.display.update()
-				time.sleep(0.01)
+		self.surface.blit(labelmax, (self.margin_l, 10))
+		self.surface.blit(labelmin, (self.margin_l, 70))
+		self.display.blit(self.surface, (0, self.nb*self.height))
+		pygame.display.update()
+		Channel.nb = Channel.nb+1
 
-		pygame.draw.lines(self.surface, self.color, False, cap_tuples, 2)
-		self.display.blit(self.surface, (0, self.nb*Channel.height))
+	def plot(self):
+		scaled_vals = []
+		for tup in self.values:
+			x_scaled = tup[0]*self.x_scale
+			y_scaled = (tup[1]-self.min)/((self.max-self.min)+0.01)*self.height
+			scaled_vals.append((x_scaled+self.margin_l, y_scaled))
+		pygame.draw.lines(self.surface, self.color, False, scaled_vals, 2)
+		self.display.blit(self.surface, (0, self.nb*self.height))
 		pygame.display.update()
 	def reset():
 		Channel.nb = 0
@@ -91,6 +84,7 @@ def button(disp, x_pos, y_pos, width, height, text, arg, action=None):
 			pos = pygame.mouse.get_pos()
 	if x_pos+width > pos[0] > x_pos and y_pos+height > pos[1] > y_pos:
 		pygame.draw.rect(disp, Colors.yello,(x_pos,y_pos,width,height))
+		pygame.display.update()
 		action(arg)
 
 	pygame.font.init()
@@ -102,10 +96,10 @@ def button(disp, x_pos, y_pos, width, height, text, arg, action=None):
 def disp_default_chans(screen):
 	Channel.reset()
 	''' Creates empty channels to display before any capture is made'''
-	Channel(screen, [0,0], Colors.brown, 	"ms").plot()
-	Channel(screen, [0,0], Colors.red, 	"ms").plot()
-	Channel(screen, [0,0], Colors.yello, 	"ms").plot()
-	Channel(screen, [0,0], Colors.orang, 	"ms").plot()
+	Channel(screen, [(0,0),(0,0)], Colors.brown, 	"ms")
+	Channel(screen, [(0,0),(0,0)], Colors.red, 		"ms")
+	Channel(screen, [(0,0),(0,0)], Colors.yello, 	"ms")
+	Channel(screen, [(0,0),(0,0)], Colors.orang, 	"ms")
 	pygame.display.update()
 
 def plot_capture(screen):
@@ -116,11 +110,15 @@ def plot_capture(screen):
 	vals_ch2 = []
 	vals_ch3 = []
 	capture = busPirate.capture_voltage()
-	for capture_slice in capture:
-		vals_ch0.append(float(capture_slice['BR']))
-		vals_ch1.append(float(capture_slice['RD']))
-		vals_ch2.append(float(capture_slice['YW']))
-		vals_ch3.append(float(capture_slice['OR']))
+	surf = pygame.Surface((screen.get_width(), 50))
+
+	# Create a tuple list for each channel
+	for id, capture_slice in enumerate(capture):
+		vals_ch0.append((id, float(capture_slice['BR'])))
+		vals_ch1.append((id, float(capture_slice['RD'])))
+		vals_ch2.append((id, float(capture_slice['YW'])))
+		vals_ch3.append((id, float(capture_slice['OR'])))
+	# Plot them
 	Channel(screen, vals_ch0, Colors.brown,	"ms").plot()
 	Channel(screen, vals_ch1, Colors.red, 	"ms").plot()
 	Channel(screen, vals_ch2, Colors.yello,	"ms").plot()
